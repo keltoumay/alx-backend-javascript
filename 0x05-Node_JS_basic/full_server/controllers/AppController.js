175318dfb698d5aca4d8aea72966d6586fa1f8a3
keltoumay <keltoumaouyahia@gmail.com>
2024-06-21 14:56:53 -0700

    Node_JS

--- 0x05-Node_JS_basic/full_server/controllers/AppController.js
+++ 0x05-Node_JS_basic/full_server/controllers/AppController.js
@@ -1,5 +1,11 @@
-export class AppController {
-  static getHomepage(req, res) {
-    res.status(200).send('Hello Holberton School!');
+/**
+ * Contains the miscellaneous route handlers.
+ */
+class AppController {
+  static getHomepage(request, response) {
+    response.status(200).send('Hello Holberton School!');
   }
 }
+
+export default AppController;
+module.exports = AppController;
